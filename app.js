512774453369e799b8b414b590aba1d31ef325b4
luca-conti <lucagopro12@gmail.com>
2024-10-15 11:46:59 -0300

    concluido

--- app.js
+++ app.js
@@ -1,36 +1,59 @@
-//window('ola esse é um programa desenvolvido onde o obigetivo é vc digitar um numero //escolhido pro vc e o programa fazer uma divisão ou soma ou multiplicação ou subritação')
-//let pergunda = Number(prompt("digite um numero pra ser somado ou dividido multiplicado ou //subritaido aleatoriamente"))
-//let pergundadmss = Number(prompt("digite outro para ser somado ou dividido multiplicado //oupergunda aleatoriamente"))
-//let DMSS = parseInt(Math.random() * 4 + 1)
+let DMSS = parseInt(Math.random() * 4 + 1)
 
 function numero1() {
     let primeiroNumero = document.getElementById('primeiroNumero').value
     console.log(`${primeiroNumero}`)
+    let segundoNumero = document.getElementById('segundoNumero').value
+    console.log(`${segundoNumero}`)
+
+if (DMSS == 1) {
+    let soma =  primeiroNumero + segundoNumero
+    exibir =(`a soma de ${primeiroNumero} com ${segundoNumero} deu ${soma}`);
+    exbirMensagemInicial(exibir)
+} else if (DMSS == 2) {
+    let subritair = primeiroNumero - segundoNumero
+    exibir =(`a subritação de ${primeiroNumero} com ${segundoNumero} deu ${subritair}`);
+    exbirMensagemInicial(exibir)
+    } else  if (DMSS == 3) {
+        let multiplicado = primeiroNumero * segundoNumero
+        exibir =(`a multiplicação de ${primeiroNumero} com ${segundoNumero} deu ${multiplicado}`);
+        exbirMensagemInicial(exibir)
+        } else {
+            let dividido = primeiroNumero / segundoNumero
+            exibir =(`a divisão de ${primeiroNumero} com ${segundoNumero} deu ${dividido}`);
+            exbirMensagemInicial(exibir)
+        }
+        
 }
+
+
+
 
 
 let exibir = ``
 
+//let soma = numero1() + numero2();
+
     
 
- if (DMSS == 1) {
-        let soma = numero1() + pergundadmss
-        exibir =`a soma de ${pergunda} com ${pergundadmss} deu ${soma}`;
-        exbirMensagemInicial(exibir)
-    } else if (DMSS == 2) {
-        let subritair = pergunda - pergundadmss
-        exibir =`a subritação de ${pergunda} com ${pergundaDmss} deu ${subritair}`;
-        exbirMensagemInicial(exibir)
-        } else  if (DMSS == 3) {
-            let multiplicado = pergunda * pergundadmss
-            exibir =`a multiplicação de ${pergunda} com ${pergundadmss} deu ${multiplicado}`;
-            exbirMensagemInicial(exibir)
-             } else {
-                let dividido = pergunda / pergundadmss
-                exibir =`a divisão de ${pergunda} com ${pergundadmss} deu ${dividido}`;
-                exbirMensagemInicial(exibir)
-            }
-        
+
+  //  if (DMSS == 1) {
+  //          let soma =  
+  //          exibir =(`a soma de ${numero1()} com ${numero2()} deu ${soma}`);
+  //          exbirMensagemInicial(exibir)
+  //      } else if (DMSS == 2) {
+  //          let subritair = numero1() - numero2()
+  //          exibir =(`a subritação de ${numero1()} com ${numero2()} deu ${subritair}`);
+  //          exbirMensagemInicial(exibir)
+  //          } else  if (DMSS == 3) {
+  //              let multiplicado = numero1() * numero2()
+  //              exibir =(`a multiplicação de ${numero1()} com ${numero2()} deu ${multiplicado}`);
+  //              exbirMensagemInicial(exibir)
+  //              } else {
+  //                  let dividido = numero1() / numero2()
+  //                  exibir =(`a divisão de ${numero1()} com ${numero2()} deu ${dividido}`);
+  //                  exbirMensagemInicial(exibir)
+  //              }
 
 
 function exbirTextoNaTela(tag, texto) {
@@ -38,6 +61,8 @@ function exbirTextoNaTela(tag, texto) {
     campo.innerHTML = texto;
 }
 function exbirMensagemInicial() {
-    exbirTextoNaTela('h1', exibir);}
+    exbirTextoNaTela('h1', exibir);
+}
+
 
 
