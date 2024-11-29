70c54142fd879bc2dcdcf903d21289c1ce598994
luca-conti <lucagopro12@gmail.com>
2024-11-29 09:12:15 -0300

    reiniciar

--- app.js
+++ app.js
@@ -8,27 +8,37 @@ function numero1() {
 
 if (DMSS == 1) {
     let soma =  primeiroNumero + segundoNumero
-    exibir =(`a soma de ${primeiroNumero} com ${segundoNumero} deu ${soma}`);
+    exibir =(`A soma de ${primeiroNumero} com ${segundoNumero} deu ${soma}`);
     exbirMensagemInicial(exibir)
 } else if (DMSS == 2) {
     let subritair = primeiroNumero - segundoNumero
-    exibir =(`a subritação de ${primeiroNumero} com ${segundoNumero} deu ${subritair}`);
+    exibir =(`A subritação de ${primeiroNumero} com ${segundoNumero} deu ${subritair}`);
     exbirMensagemInicial(exibir)
     } else  if (DMSS == 3) {
         let multiplicado = primeiroNumero * segundoNumero
-        exibir =(`a multiplicação de ${primeiroNumero} com ${segundoNumero} deu ${multiplicado}`);
+        exibir =(`A multiplicação de ${primeiroNumero} com ${segundoNumero} deu ${multiplicado}`);
         exbirMensagemInicial(exibir)
         } else {
             let dividido = primeiroNumero / segundoNumero
-            exibir =(`a divisão de ${primeiroNumero} com ${segundoNumero} deu ${dividido}`);
+            exibir =(`A divisão de ${primeiroNumero} com ${segundoNumero} deu ${dividido}`);
             exbirMensagemInicial(exibir)
         }
+
+        alterarStatus()
         
 }
 
-
-
-
+function alterarStatus() {
+    let btnReniciar = document.getElementById("btn-reniciar");
+    
+    if (btnReniciar.classList.contains('reiniciar')) {
+       btnReniciar.classList.remove('reiniciar')
+       btnReniciar.classList.add('botao1')
+    } else {
+        btnReniciar.classList.add('reiniciar')
+        btnReniciar.classList.remove('botao1')
+    }
+}
 
 let exibir = ``
 function exbirTextoNaTela(tag, texto) {
@@ -39,5 +49,11 @@ function exbirMensagemInicial() {
     exbirTextoNaTela('h1', exibir);
 }
 
+ function reiniciar() {
+        document.getElementById('primeiroNumero').value = '';
+        document.getElementById('segundoNumero').value = '';
+        DMSS = parseInt(Math.random() * 4 + 1)
+        innerHTML = '<h1 class="titulo">DIGITE O NUMERO ALEATORIO</h1>'
 
-
+        alterarStatus()
+ }
